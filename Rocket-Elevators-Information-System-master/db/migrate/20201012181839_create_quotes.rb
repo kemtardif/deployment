3ad9c3ac38ff7444586e4ea3cc96466d2ebd46cf
kngBMermaid <louis.marmen.92@gmail.com>
2020-10-12 19:24:51 -0400

    quote table completed

--- Rocket-Elevators-Information-System-master/db/migrate/20201012181839_create_quotes.rb
+++ Rocket-Elevators-Information-System-master/db/migrate/20201012181839_create_quotes.rb
@@ -1,10 +1,22 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :quotes do |t|
+      t.string :userName
       t.string :buildingType
-      t.string
-      t.decimal 
-      t.decimal :urmama
+      t.integer :apartmentNumbers
+      t.integer :numberOfFloors
+      t.integer :numberOfBasements
+      t.integer :numberOfStore
+      t.integer :numberCages
+      t.integer :parkingLevels
+      t.integer :numberOfCompanies
+      t.integer :occupantsPerFloor
+      t.integer :estimatedCagesNeeded
+      t.string :packageSelection
+      t.decimal :elevatorPrice, precision:10, scale:2
+      t.decimal :installationCost, precision:10, scale:2
+      t.decimal :totalPrice, precision:10, scale:2
+      t.timestamps
     end
   end
 end
